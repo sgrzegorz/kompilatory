f59b073902446717e5529646d3e423f5e527915b
sgrzegorz <grzegorzsroka2@gmail.com>
2020-06-05 11:11:53 +0200

    updated main

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import sys
 import Mparser
 import scanner
 from TypeChecker import TypeChecker
+from Interpreter import Interpreter
+
 
 if __name__ == '__main__':
     # control_transfer
     # init
     # opers
     filename = sys.argv[1] if len(sys.argv) > 1 else "init.m"
+    filename ="data/"+filename
     try:
         file = open(filename)
     except IOError:
@@ -24,3 +27,9 @@ if __name__ == '__main__':
         print('start treewalk')
         typeChecker = TypeChecker()
         typeChecker.visit(ast)   # or alternatively ast.accept(typeChecker)
+
+        ast.accept(Interpreter())
+        # in future
+        # ast.accept(OptimizationPass1())
+        # ast.accept(OptimizationPass2())
+        # ast.accept(CodeGenerator())
