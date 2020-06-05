a79154363770f15edfda0513b3b7bf8d048c3e05
sgrzegorz <grzegorzsroka2@gmail.com>
2020-06-05 11:53:45 +0200

    addaed data

---
+++ data/example1.m
@@ -0,0 +1,47 @@
+# special functions, initializations
+
+return 5;
+
+B = ones(7, 4,5,6);   # create 7x7 matrix filled with ones
+
+#x = eye(5);
+#y = eye(8);
+#z = x + y;
+
+y = eye(5);
+x = eye(5);
+z = x + y;
+#+ zeros(5);
+
+#a = [1,2,3];
+#b = [1,3,5];
+#b += [1,3,5];
+#a += b;
+
+#a = 5;
+#b = 4;
+#c = a + b;
+#c += a;
+#a += b + c;
+# if(5>=a) {
+#     print "N>5";
+# }
+#
+#a = [1,2,3];
+#a -= [1,2,3];
+
+#d = 5;
+#e = [4,5];
+#g = [2,3];
+#d /= e + g;
+
+#a = 5 + 7 + 9 += 9; # a co z tym paskudztwem? chyba nic
+#a = 5 + 6 + 7;
+#b = [1,2,3];
+#a += b;
+
+#a = [1,2];
+# b = [1,2,3];
+#a += b;
+
+#a = b;
