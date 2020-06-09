72792509fbf24dafd9b8f5bdc0addcf266d2f818
Monika Dziedzic <moni.dziedzic@gmail.com>
2020-06-10 00:16:51 +0200

    Fixed for.m test

--- data/for.m
+++ data/for.m
@@ -1,63 +1,64 @@
-#k=-3;
-#b=-2;
-#for j = 1:10
-#    print j,k,b;
+k=-3;
+b=-2;
+for j = 1:10
+    print j,k,b;
 
-#k=2;
-#while(k>0) {
- #   k = k - 1;
-#}
+k=2;
+while(k>0) {
+   k = k - 1;
+}
 
-#print 8, 9;
+print 8, 9;
 
 
 
 
 #############################
 
-#N =9;
-#M=10;
-#if(N>5) {
- #   print "N>5";
-#}
-#else if(N>=0) {
- #   print "N>=0";
-#}
+N =9;
+M=10;
+if(N>5) {
+   print "N>5";
+}
+else if(N>=0) {
+   print "N>=0";
+}
 
-#if(N<10) {
- #   print "N<10";
-#}
-#else if(N<=15)
- #   print "N<=15";
+if(N<10) {
+   print "N<10";
+}
+else if(N<=15)
+   print "N<=15";
 
-#k = 10;
-#while(k>0)
- #   k = k - 1;
+k = 10;
+while(k>0)
+   k = k - 1;
 
-#while(k>0) {
- #   if(k<5)
-  #      i = 1;
-   # else if(k<10)
-    #    i = 2;
-    #else
-    #    i = 3;
+while(k>0) {
+    if(k<5)
+      i = 1;
+    else if(k<10)
+        i = 2;
+    else
+       i = 3;
 
-    #k = k - 1;
-#}
-
-
-#for i = 1:N
- # for j = i:M
-  #  print i, j;
+    k = k - 1;
+}
 
 
 for i = 1:N
+  for j = i:M
+    print i, j;
+
+
+for i = 1:N{
     if(i<=N/16) {
         print i;
+    }
+}
 
 
-
-#{
-#  N = 100;
-#  M = 200;
-#}
+{
+  N = 100;
+  M = 200;
+}
